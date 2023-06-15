b1e580c19df19debd85099f76eb0aa654914425e
AndyyudaVPN <134195192+AndyyudaVPN@users.noreply.github.com>
2023-06-15 14:25:48 +0700

    Update themes.sh

--- menu/themes.sh
+++ menu/themes.sh
@@ -1,6 +1,6 @@
 #Script Auto Reboot Vps
 #wget https://github.com/${GitUser}/
-GitUser="kenDevXD"
+GitUser="AndyyudaVPN"
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m"
