267e9c49d747ca635131a93596f06ec60bd09cf9
AndyyudaVPN <134195192+AndyyudaVPN@users.noreply.github.com>
2023-06-15 14:03:00 +0700

    Add files via upload

---
+++ system/restore.sh
@@ -0,0 +1,55 @@
+#wget https://github.com/${GitUser}/
+GitUser="kenDevXD"
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+echo -e "\e[32mloading...\e[0m"
+clear
+# Valid Script
+VALIDITY () {
+    today=`date -d "0 days" +"%Y-%m-%d"`
+    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
+    if [[ $today < $Exp1 ]]; then
+    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
+    else
+    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
+    echo -e "\e[31mPlease renew your ipvps first\e[0m"
+    exit 0
+fi
+}
+IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+VALIDITY
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+echo -e "\e[31mPlease buy script first\e[0m"
+exit 0
+fi
+echo -e "\e[32mloading...\e[0m"
+clear
+echo "This Feature Can Only Be Used According To Vps Data With This Autoscript"
+echo "Please input link to your vps data backup file."
+echo "You can check it on your email if you run backup data vps before."
+read -rp "Link File: " -e url
+wget -O backup.zip "$url"
+unzip backup.zip
+rm -f backup.zip
+sleep 1
+echo Start Restore
+cd /root/backup
+cp passwd /etc/
+cp group /etc/
+cp shadow /etc/
+cp gshadow /etc/
+cp -r wireguard /etc/
+cp ss.conf /etc/shadowsocks-libev/ss.conf
+cp -r premium-script /var/lib/
+cp -r xray /usr/local/etc/
+cp -r trojan-go /etc/
+cp -r shadowsocksr /usr/local/
+cp -r public_html /home/vps/
+cp crontab /etc/
+strt
+rm -rf /root/backup
+rm -f backup.zip
+echo Done
